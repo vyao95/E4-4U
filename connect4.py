2e468931eed7580b0eeff0256e3246359a25459b
Sota Nakajima <snakajim@ucsc.edu>
2018-12-09 21:18:35 -0800

    Changed how is_ended works. Returns tuple with (<True/False>,<player/None>)

--- connect4.py
+++ connect4.py
@@ -5,24 +5,24 @@ width = 7
 height = 6
 
 
-#Connect 4 Board Object 
+#Connect 4 Board Object
 class Board:
-    
+
     #Create new board
     def __init__(self):
         self.state = {}
         for col in range(width):
             for row in range(height):
                 self.state[col,row] = empty
-                
-    
+
+
     def print_board(self,state):
         for i in range(height-1,-1,-1):
             for j in range(0,width):
                 print(str(state[(j,i)]) + " ", end="")
             print()
         print()
-        
+
     def do_move(self,state,player,move):
         if move in state:
             if move in self.get_valid_moves(state):
@@ -34,9 +34,9 @@ class Board:
                 print("do_move: Not a valid move")
         else:
             print("do_move: Move not in board.")
-        
+
         return state
-            
+
     #Get moves you can do for next turn
     def get_valid_moves(self,state):
         valid_moves = []
@@ -46,40 +46,43 @@ class Board:
                     valid_moves.append((col,row))
                     break
         return valid_moves
-        
+
     #check the valid moves and see if any are moves that will make you win
-    def get_winning_moves(self,state,turn,valid_moves=[]):
+    def get_winning_moves(self,state,valid_moves=[]):
         winning_moves = []
         if len(valid_moves) == 0: # this will let us use our own valid moves if we choose to optimize stuff
             valid_moves = self.get_valid_moves(state)
         for move in valid_moves:
+            turn = state[move]
+            if turn == '.':
+                continue
             winnable = 0
             # check vertical win
             if move[1] > 2:
                 for i in range(1,4):
                     if (state[(move[0],move[1]-i)] == turn):
                         if winnable == 2:
-                            winning_moves.append(move)
+                            winning_moves.append((move,turn))
                         else:
                             winnable += 1
-                    else: 
+                    else:
                         break
-        
+
             winnable = 0
             #horizontal win
             for k in range(-1,2,2): # check to both left and right
                 if move not in winning_moves: # haven't already found win from one direction
-                    for i in range(1,4): 
+                    for i in range(1,4):
                         if (move[0]-i*k,move[1]) in state: # check 3 left/right
                             if (state[(move[0]-i*k,move[1])] == turn):
                                 if winnable == 2:
-                                    winning_moves.append(move)
+                                    winning_moves.append((move,turn))
                                     break
                                 else:
                                     winnable += 1
                             else:
                                 break
-                                
+
             winnable = 0
             # / win
             for k in range(-1,2,2): # check bottom left and top right directions
@@ -88,13 +91,13 @@ class Board:
                         if (move[0]-i*k,move[1]-i*k) in state:
                             if (state[(move[0]-i*k,move[1]-i*k)] == turn):
                                 if winnable == 2:
-                                    winning_moves.append(move)
+                                    winning_moves.append((move,turn))
                                     break
                                 else:
                                     winnable += 1
-                            else: 
+                            else:
                                 break
-                                
+
             winnable = 0
             # \ win
             for k in range(-1,2,2): # check bottom left and top right directions
@@ -103,82 +106,33 @@ class Board:
                         if (move[0]-i*k,move[1]+i*k) in state:
                             if (state[(move[0]-i*k,move[1]+i*k)] == turn):
                                 if winnable == 2:
-                                    winning_moves.append(move)
+                                    winning_moves.append((move,turn))
                                     break
                                 else:
                                     winnable += 1
-                            else: 
+                            else:
                                 break
-            
+
         return winning_moves
-        
+
     def is_ended(self,state):
-        if len(self.get_valid_moves(state)) == 0:
-            return True
-            
         last_moves = []
         for col in range(width):
             for row in range(height):
-                if state[col,row] == empty:
-                    if (col,row-1) in state:
+                if state[col,row] == empty: # if we reach an empty spot (going up vertically)
+                    if (col,row-1) in state: # then add the spot right before empty spot
                         last_moves.append((col,row-1))
                     break
-                    
-        for move in last_moves:
-            turn = state[move]
-            won = 0
-            # check vertical win
-            if move[1] > 2:
-                for i in range(1,4):
-                    if (state[(move[0],move[1]-i)] == turn):
-                        if won == 2:
-                            return True
-                        else:
-                            won += 1
-                    else: 
-                        break
-        
-            won = 0
-            #horizontal win
-            for k in range(-1,2,2): # check to both left and right
-                for i in range(1,4): 
-                    if (move[0]-i*k,move[1]) in state: # check 3 left/right
-                        if (state[(move[0]-i*k,move[1])] == turn):
-                            if won == 2:
-                                return True
-                            else:
-                                won += 1
-                        else:
-                            break
-                                
-            won = 0
-            # / win
-            for k in range(-1,2,2): # check bottom left and top right directions
-                for i in range(1,4):
-                    if (move[0]-i*k,move[1]-i*k) in state:
-                        if (state[(move[0]-i*k,move[1]-i*k)] == turn):
-                            if won == 2:
-                                return True
-                            else:
-                                won += 1
-                        else: 
-                            break
-                                
-            won = 0
-            # \ win
-            for k in range(-1,2,2): # check bottom left and top right directions
-                for i in range(1,4):
-                    if (move[0]-i*k,move[1]+i*k) in state:
-                        if (state[(move[0]-i*k,move[1]+i*k)] == turn):
-                            if won == 2:
-                                return True
-                            else:
-                                won += 1
-                        else: 
-                            break
-        return False
-            
-        
+                elif row == height - 1: # if we reach the top
+                    last_moves.append((col,row))
+
+        won = self.get_winning_moves(state,last_moves)
+        if len(won) == 0:
+            return (False,None)
+        else:
+            return (True,won[0][1])
+
+
 b = Board()
 for i in range(0,3):
     for j in range(0,3-i):
@@ -190,5 +144,5 @@ b.state[3,0] = player
 b.print_board(b.state)
 
 print(b.get_valid_moves(b.state))
-print(b.get_winning_moves(b.state,player))
+print(b.get_winning_moves(b.state))
 print(b.is_ended(b.state))
