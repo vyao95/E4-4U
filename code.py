3a583d0187f2c400f3d8329c14a7a2911633f0c4
Sota Nakajima <snakajim@ucsc.edu>
2018-12-12 17:38:56 -0800

    User click positions saved for the connect 4 board itself

--- code.py
+++ code.py
@@ -1,45 +1,46 @@
 from PIL import ImageGrab
-from win32 import win32api, win32con
+from pynput.mouse import Button, Controller, Listener
 import os
 import time
 
-x_pad = 540
-y_pad = 430
 
-x_size = 2280
-y_size = 1290
+mouse = Controller()
 
-board_x = 7
-board_y = 6
 
-def screenGrabBox():
-    box = (x_pad,y_pad,x_pad+x_size,y_pad+y_size)
+board_width = 7
+board_height = 6
+
+
+coordinates = []
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        coordinates.append((x,y))
+    if len(coordinates) == 2:
+        # Stop listener
+        return False
+
+def screen_grab_box():
+    box = (x_pad, y_pad,x_pad + x_size, y_pad + y_size)
     im = ImageGrab.grab(box)
     im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
 '.png', 'PNG')
 
 
-# Mouse functionality
-def leftClick():
-	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-	time.sleep(.1)
-	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-	print("Click.")
-
-def mousePos(cord):
-	win32api.SetCursorPos(x_pad + cord[0], y_pad + cord[1])
-
-def get_cords():
-	x,y = win32api.GetCursorPos()
-	x = x - x_pad
-	y = y - y_pad
-	print(x,y)
-
-# def startGame():
-# 	mousePos(())
- 
-def main():
-    screenGrabBox()
+def left_click(x, y):
+    mouse.move(x,y)
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+    
+def get_board_coordinates():    
+    # Collect click events until on_click returns False
+    with Listener(on_click=on_click) as listener:
+        listener.join()
+    
+# def main():
  
 if __name__ == '__main__':
-    main()
+    print("Please click top left and bottom right of the Connect 4 Board on your turn.")
+
+    get_board_coordinates()
+    print(coordinates)
